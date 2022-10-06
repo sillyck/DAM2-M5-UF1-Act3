0ae39a8b77a6f50c46a29f805d291c9c5e24cd70
Jordi Ribellas <ribellas2002@gmail.com>
2022-10-06 15:44:16 +0200

    Tauler de 16 per 16 creat

--- src/main.java
+++ src/main.java
@@ -1,6 +1,16 @@
 public class main {
-
+	
+	private static char[][] prada = new char[16][16];
+	
 	public static void main(String[] args) {
+		
+		for(int i=0;i<16;i++) {
+			for (int j=0; j<16;j++) {
+				prada[i][j] = '#';
+				System.out.print(prada[i][j] + " ");
+			}
+			System.out.println("");
+		}
 		
 
 	}
