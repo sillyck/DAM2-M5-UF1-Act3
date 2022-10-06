b9380474b38d13ece581ef62f0f2e37373163624
Jordi Ribellas <ribellas2002@gmail.com>
2022-10-06 17:14:15 +0200

    primers pasos de convertir la formula llarga a codi

--- src/main.java
+++ src/main.java
@@ -1,17 +1,23 @@
 public class main {
-	
-	private static char[][] prada = new char[16][16];
-	
+
 	public static void main(String[] args) {
-		
-		for(int i=0;i<16;i++) {
-			for (int j=0; j<16;j++) {
-				prada[i][j] = '#';
-				System.out.print(prada[i][j] + " ");
+
+		int v = 0, t = 0, i = 0, j = 0, a = 1, o = 2, u = 2;
+
+		int[][] ovella = new int[20][10];
+
+		int random1 = (int) (-4 + Math.random() * 8);
+		int random2 = (int) (-4 + Math.random() * 8);
+
+		int p = 0;
+
+		for (t = 0; t < 20; t++) { // ronda
+			for (i = 0; i < 10; i++) { // ovella numero
+				for (j = 1; j < 3; j++) { // posicio vertical o horitzontal
+					
+				}
 			}
-			System.out.println("");
 		}
-		
 
 	}
 
